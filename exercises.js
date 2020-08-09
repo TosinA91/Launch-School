da6aa2f164ac55933612df5b8bc3f9a8d2ac388a
Tosin Adeyemo <Td.adeyemo@gmail.com>
2020-08-09 16:30:28 -0400

    added two exercises

--- exercises.js
+++ exercises.js
@@ -66,3 +66,20 @@ let pets = {'bingo': dog, 'sandy': cat, 'dusty': cat, }
 // What value does the following expression evaluate to?
 
 // '12' < '9' - true
+
+//Write a program name greeter.js that greets 'Victor' three times. Your program should use a variable and not hard code the string value 'Victor' in each greeting. Here's an example run of the program://
+
+
+let name = 'Victor';
+console.log(`Good Morning, ${name}.`);
+console.log(`Good Afternoon, ${name}.`);
+console.log(`Good Evening, ${name}.`);
+
+//Write a program named age.js that includes someone's age and then calculates and reports the future age in 10, 20, 30 and 40 years. Below is the output for someone 20 years old.//
+
+let age = 20;
+console.log(`You are ${age} years old.`)
+console.log(`In 10 years, you will be ${age + 10} years old.`);
+console.log(`In 20 years, you will be ${age + 20} years old.`)
+console.log(`In 30 years, you will be ${age + 30} years old.`)
+console.log(`In 40 years, you will be ${age + 40} years old.`)
