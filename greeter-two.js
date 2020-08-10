353916b86cf60ac31425f5fc1ca52cbfd42f79d5
Tosin Adeyemo <Td.adeyemo@gmail.com>
2020-08-10 19:34:35 -0400

    added input/output exercises

---
+++ greeter-two.js
@@ -0,0 +1,5 @@
+let rlSync = require('readline-sync');
+let firstName = rlSync.question('What is your firstname?\n')
+let lastName = rlSync.question('What is your last name?\n')
+
+console.log(`Hello ${firstName} ${lastName}!`)
