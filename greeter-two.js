27ab133e9cfc5a2248bf4dc409b0079f687518ad
Tosin Adeyemo <Td.adeyemo@gmail.com>
2020-08-10 19:48:06 -0400

    added age exercise

--- greeter-two.js
+++ greeter-two.js
@@ -1,5 +1,5 @@
 let rlSync = require('readline-sync');
-let firstName = rlSync.question('What is your firstname?\n')
-let lastName = rlSync.question('What is your last name?\n')
+let firstName = rlSync.question('What is your firstname?\n');
+let lastName = rlSync.question('What is your last name?\n');
 
-console.log(`Hello ${firstName} ${lastName}!`)
+console.log(`Hello ${firstName} ${lastName}!`);
