0ef415d903ce13a11b49b65b045a289f55b29bd9
Tosin Adeyemo <Td.adeyemo@gmail.com>
2020-08-11 17:02:05 -0400

    add function exercises

---
+++ multiply.js
@@ -0,0 +1,12 @@
+function multiply(a, b) {
+    return a * b;
+}
+
+function getNumber(prompt) {
+    let readlineSync = require('readline-sync');
+    return parseFloat(readlineSync.question(prompt));
+}
+
+let firstNumber = getNumber('Enter the first number:')
+let secondNumber = getNumber('Enter the second number:')
+console.log(`${firstNumber} * ${secondNumber} = ${multiply(firstNumber, secondNumber)}`)
